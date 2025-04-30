bd1c30bc543c8c86a0a8040b90250a28a873ed00
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2025-04-30 22:56:35 +0700

    Clean up - part 1 (#1)
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,7 @@
 import { defineConfig } from "astro/config";
-import { FontaineTransform } from "fontaine";
 
-const site = "https://slcdts.netlify.app";
+const site = process.env.URL || "http://localhost:4321";
 
-// https://astro.build/config
 export default defineConfig({
-  site: process.env.CI ? site : "http://localhost:4321",
-  vite: {
-    plugins: [
-      FontaineTransform.vite({
-        fallbacks: ["Arial"],
-        resolvePath: (id) => new URL(`./public${id}`, import.meta.url),
-      }),
-    ],
-  },
+  site,
 });
